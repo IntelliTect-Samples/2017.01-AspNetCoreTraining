86de232de9a14aeb6022702b9e192e6113473b42
Grant Erickson <grant@intellitect.com>
2017-01-08 14:21:14 -0800

    Added Gifts Controller Updated _Layout for scripts Added _ValidationScriptsPartial Added jquery validation and unobtrusive

---
+++ src/SecretPal.Web/Controllers/GiftsController.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SecretPal.Data;
+using SecretPal.Data.Models;
+
+namespace SecretPal.Web.Controllers
+{
+    public class GiftsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public GiftsController(AppDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Gifts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Gifts.ToListAsync());
+        }
+
+        // GET: Gifts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gift = await _context.Gifts
+                .SingleOrDefaultAsync(m => m.GiftId == id);
+            if (gift == null)
+            {
+                return NotFound();
+            }
+
+            return View(gift);
+        }
+
+        // GET: Gifts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Gifts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("GiftId,Name,Cost")] Gift gift)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(gift);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(gift);
+        }
+
+        // GET: Gifts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gift = await _context.Gifts.SingleOrDefaultAsync(m => m.GiftId == id);
+            if (gift == null)
+            {
+                return NotFound();
+            }
+            return View(gift);
+        }
+
+        // POST: Gifts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("GiftId,Name,Cost")] Gift gift)
+        {
+            if (id != gift.GiftId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(gift);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GiftExists(gift.GiftId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(gift);
+        }
+
+        // GET: Gifts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gift = await _context.Gifts
+                .SingleOrDefaultAsync(m => m.GiftId == id);
+            if (gift == null)
+            {
+                return NotFound();
+            }
+
+            return View(gift);
+        }
+
+        // POST: Gifts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var gift = await _context.Gifts.SingleOrDefaultAsync(m => m.GiftId == id);
+            _context.Gifts.Remove(gift);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool GiftExists(int id)
+        {
+            return _context.Gifts.Any(e => e.GiftId == id);
+        }
+    }
+}
