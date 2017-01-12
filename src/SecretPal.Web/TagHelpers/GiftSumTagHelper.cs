80ba0dc532635a691471b3191042e2d7dec8ba9f
Grant Erickson <grant@intellitect.com>
2017-01-11 22:07:04 -0800

    Added Tag Helper to get the sum from the database.

---
+++ src/SecretPal.Web/TagHelpers/GiftSumTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using SecretPal.Data;
+using System.Threading.Tasks;
+
+namespace SecretPal.Web.TagHelpers
+{
+    public class GiftSumTagHelper : TagHelper
+    {
+        private AppDbContext DataContext { get; }
+
+        /// <summary>
+        /// Divide the sum by this number
+        /// </summary>
+        public decimal MultiplicationFactor { get; set; }
+
+        public GiftSumTagHelper(AppDbContext dataContext)
+        {
+            // This gets Dependency Injected
+            DataContext = dataContext;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "p";
+            var sum = await DataContext.Gifts.SumAsync(f => f.Cost);
+            output.Content.SetContent($"Total Gifts: {sum * MultiplicationFactor}");
+        }
+    }
+}
